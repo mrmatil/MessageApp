6553ef49a300ab0fdc0cb322e2d9abc36b8bc69d
mrmatil <niedzielnygracz1234@gmail.com>
2019-07-14 18:35:15 +0200

    Text field and table view in chat View Controller are working now

--- MessageApp/Controller/Chat.swift
+++ MessageApp/Controller/Chat.swift
@@ -11,6 +11,9 @@ import UIKit
 class Chat: UIViewController {
 
     //IBOutlets:
+    @IBOutlet weak var messagesTableView: UITableView!
+    @IBOutlet weak var messagesTextField: UITextField!
+    @IBOutlet weak var textViewHeight:NSLayoutConstraint!
     
     
     //IBActions
@@ -23,12 +26,18 @@ class Chat: UIViewController {
     //Functions:
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        messagesTableView.delegate=self
+        messagesTableView.dataSource=self
+        messagesTextField.delegate=self
+        messagesTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        let tapRegistered = UITapGestureRecognizer(target: self, action: #selector(tableViewTap))
+        messagesTableView.addGestureRecognizer(tapRegistered)
+        changeCellHeight()
         // Do any additional setup after loading the view.
     }
     
 
-
+    
 
 }
 
@@ -45,4 +54,50 @@ extension Chat{
             print(error.localizedDescription)
         }
     }
+}
+
+//tableView stuff:
+extension Chat: UITableViewDelegate, UITableViewDataSource{
+    
+    func changeCellHeight(){
+//        messagesTableView.rowHeight = UITableView.automaticDimension
+//        messagesTableView.estimatedRowHeight = 120.0
+        messagesTableView.rowHeight=120.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! MessageCell
+        
+        let testArray:[String] = ["test1","test2","test3"]
+        cell.messageLabel.text=testArray[indexPath.row]
+        return cell
+    }
+    
+    @objc func tableViewTap(){
+        messagesTextField.endEditing(true)
+    }
+    
+}
+
+//message TextField Stuff:
+extension Chat: UITextFieldDelegate{
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.2) {
+            self.textViewHeight.constant = 265+50
+            self.view.layoutIfNeeded() // refreshes layout
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.2) {
+            self.textViewHeight.constant = 50
+            self.view.layoutIfNeeded() // refreshes layout
+        }
+    }
+    
 }
